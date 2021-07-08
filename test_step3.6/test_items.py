f0ae7b26093baa8ea126e745601f0eae07a75e30
AntonPenkin <razvetchik5@gmail.com>
2021-07-08 15:44:58 +0300

    1. Дописал --language в conftest.py 2. Написал тест

---
+++ test_step3.6/test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_find_the_add_to_cart_button(browser):
+    browser.get(link)
+    time.sleep(5)
+    button = browser.find_element_by_css_selector('button.btn.btn-lg.btn-primary.btn-add-to-basket.bnb')
+    assert button, "No such item"
