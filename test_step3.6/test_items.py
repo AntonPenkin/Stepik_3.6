3f5fb30d9c3362fc7678305591a8bf5a48122621
AntonPenkin <razvetchik5@gmail.com>
2021-07-09 08:42:15 +0300

    Написал тест на поиск кнопки "Добавить в корзину"

--- test_step3.6/test_items.py
+++ test_step3.6/test_items.py
@@ -3,8 +3,7 @@ import time
 
 link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_find_the_add_to_cart_button(browser):
+def test_search_add_to_cart_button(browser):
     browser.get(link)
-    time.sleep(5)
-    button = browser.find_element_by_css_selector('button.btn.btn-lg.btn-primary.btn-add-to-basket.bnb')
-    assert button, "No such item"
+    #time.sleep(30)
+    assert browser.find_elements_by_css_selector('button.btn.btn-lg.btn-primary.btn-add-to-basket'), "Add to cart button not found"
